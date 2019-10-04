bdf03ae54cc3ec23f480e1aa67b1199c45efbb81
dandrusiv <dandrusiv@homeaway.com>
2019-10-04 17:09:26 +0300

    First commit

---
+++ src/main/java/com/demo/grpc/studentservice/client/ServiceStudentClient.java
@@ -0,0 +1,49 @@
+package com.demo.grpc.studentservice.client;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.stereotype.Component;
+
+import com.google.protobuf.Empty;
+import com.grpc.dao.student.service.Student;
+import com.grpc.dao.student.service.StudentStatusRequest;
+import com.grpc.student.service.StudentAgeRequest;
+import com.grpc.student.service.StudentIdRequest;
+import com.grpc.student.service.StudentServiceGrpc;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+
+@Component
+public class ServiceStudentClient {
+
+    private StudentServiceGrpc.StudentServiceBlockingStub serviceBlockingStub;
+    private StudentServiceGrpc.StudentServiceStub stub;
+
+    public ServiceStudentClient() {
+        ManagedChannel channel = ManagedChannelBuilder
+            .forAddress("localhost", 6063).usePlaintext().build();
+        serviceBlockingStub = StudentServiceGrpc.newBlockingStub(channel);
+        stub = StudentServiceGrpc.newStub(channel);
+    }
+
+    public Student getStudentById(String id) {
+        return serviceBlockingStub.getStudentById(StudentIdRequest.newBuilder().setId(id).build()).getStudent();
+    }
+
+    // dead line test
+    public List<Student> getStudentByAge(int age, int deadline) {
+        return serviceBlockingStub.withDeadlineAfter(deadline, TimeUnit.MILLISECONDS).findStudentsByAge(StudentAgeRequest.newBuilder().setAge(age).build()).getStudentsList();
+    }
+
+    public List<Student> getAllStudents() {
+        return serviceBlockingStub.findAllStudent(Empty.newBuilder().build()).getStudentsList();
+    }
+
+    public StreamObserver<StudentStatusRequest> findStudentByStatusStream(StreamObserver<Student> responseObserver) {
+        return stub.findStudentByStatusStream(responseObserver);
+    }
+
+}
---
+++ src/main/java/com/demo/grpc/studentservice/service/StudentService.java
@@ -0,0 +1,79 @@
+package com.demo.grpc.studentservice.service;
+
+import java.util.List;
+
+import org.lognet.springboot.grpc.GRpcService;
+
+import com.grpc.dao.student.service.Student;
+import com.grpc.dao.student.service.StudentStatusRequest;
+import com.grpc.dao.student.service.client.ServiceStudentDaoClient;
+import com.grpc.student.service.StudentAgeRequest;
+import com.grpc.student.service.StudentIdRequest;
+import com.grpc.student.service.StudentResponse;
+import com.grpc.student.service.StudentServiceGrpc;
+import com.grpc.student.service.StudentsResponse;
+
+import io.grpc.stub.ServerCallStreamObserver;
+import io.grpc.stub.StreamObserver;
+
+@GRpcService
+public class StudentService extends StudentServiceGrpc.StudentServiceImplBase {
+
+    private ServiceStudentDaoClient daoClient = new ServiceStudentDaoClient();
+    private static final int TIME_OUT = 2000;
+
+    @Override
+    public void getStudentById(StudentIdRequest request, StreamObserver<StudentResponse> responseObserver) {
+        System.out.println("call getStudentById");
+
+        Student studentById = daoClient.getById(request.getId());
+
+        responseObserver.onNext(StudentResponse.newBuilder().setStudent(studentById).build());
+        responseObserver.onCompleted();
+
+        System.out.println("finished work getStudentById");
+    }
+
+    @Override
+    public void findStudentsByAge(StudentAgeRequest request, StreamObserver<StudentsResponse> responseObserver) {
+        System.out.println("call findStudentsByAge");
+
+        try {
+            //emulation deadline propagation
+            System.out.println("Time out:" + TIME_OUT + " ms");
+            Thread.sleep(TIME_OUT);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ServerCallStreamObserver<StudentsResponse> streamObserver = (ServerCallStreamObserver<StudentsResponse>) responseObserver;
+        streamObserver.setOnCancelHandler(() -> System.out.println("Request was canceled"));
+
+        List<Student> studentList = daoClient.findByAge(request.getAge());
+
+        responseObserver.onNext(StudentsResponse.newBuilder().addAllStudents(studentList).build());
+        responseObserver.onCompleted();
+
+        System.out.println("finished work findStudentsByAge");
+    }
+
+    @Override
+    public void findAllStudent(com.google.protobuf.Empty request, StreamObserver<StudentsResponse> responseObserver) {
+        System.out.println("call findAllStudent");
+
+        List<Student> studentlist = daoClient.findAll();
+
+        responseObserver.onNext(StudentsResponse.newBuilder().addAllStudents(studentlist).build());
+        responseObserver.onCompleted();
+
+        System.out.println("finished work findAllStudent");
+    }
+
+    @Override
+    public StreamObserver<StudentStatusRequest> findStudentByStatusStream(StreamObserver<Student> responseObserver){
+        System.out.println("call findStudentByStatusStream");
+
+        return daoClient.findAllByStatus(responseObserver);
+    }
+
+}
